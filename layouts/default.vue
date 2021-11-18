36f0b9657cc567d5d3455a6026f14a97b1626efe
USER <EMAIL>
2021-11-18 09:14:57 +0700

    edit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <div class="bg darten-2 text-conter" align="center">
+      <span class="white--text">
+        <v-containes>
+          <Nuxt />
+        </v-containes>
+      </span>
+    </div>
+  </v-app>
+</template>
+
+<style>
+.bg{
+  /* background-color:#008cff */
+  background: linear-gradient(to top right,#111a8d,#6216f0,#4f8af7);
+}
+</style>
